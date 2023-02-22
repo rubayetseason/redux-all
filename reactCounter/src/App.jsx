ea7e6d2b1be3ff6b87d424c254f7da5c9e780204
rubayet-islam <season.ss2001@gmail.com>
2023-02-22 20:06:31 +0600

    created a store provider

--- reactCounter/src/App.jsx
+++ reactCounter/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Counter from "./Components/Counter";
 import Stats from "./Components/Stats";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 const initialState = [
   {
@@ -49,24 +51,27 @@ function App() {
   };
 
   return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-        Simple Counter Application
-      </h1>
+    <Provider store={store}>
+      {" "}
+      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
 
-      <div class="max-w-md mx-auto mt-10 space-y-5">
-        {state.map((count) => (
-          <Counter
-            key={count.id}
-            id={count.id}
-            count={count.count}
-            increment={increment}
-            decrement={decrement}
-          />
-        ))}
-        <Stats count={totalCount()} />
+        <div class="max-w-md mx-auto mt-10 space-y-5">
+          {state.map((count) => (
+            <Counter
+              key={count.id}
+              id={count.id}
+              count={count.count}
+              increment={increment}
+              decrement={decrement}
+            />
+          ))}
+          <Stats count={totalCount()} />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
--- reactCounter/src/Redux/Counter/actionTypes.js
+++ reactCounter/src/Redux/Counter/actionTypes.js
@@ -1,2 +1,2 @@
-export const INCREMENT = 'counter/increment';
-export const DECREMENT = 'counter/decrement';
+export const INCREMENT = "counter/increment";
+export const DECREMENT = "counter/decrement";
