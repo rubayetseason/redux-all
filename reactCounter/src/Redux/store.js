aa5d8fe40efba2a73a43a678a67ce7ec0378c18a
rubayet-islam <season.ss2001@gmail.com>
2023-03-07 14:10:32 +0600

    added middlewaare

--- reactCounter/src/Redux/store.js
+++ reactCounter/src/Redux/store.js
@@ -1,7 +1,14 @@
-import {createStore} from 'redux';
-import rootReducer from './rootReducer';
-rootReducer
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+// creating first middleware
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+  return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
